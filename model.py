c6e215101456ae487389d3e83b3f6bf03ffc2679
Paweł Harasiuk <pharasiuk01@gmail.com>
2024-02-08 19:35:36 +0100

    Added model

--- model.py
+++ model.py
@@ -1,9 +1,12 @@
-from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import LogisticRegression
 
 
-class NeuralNetwork:
+class Model:
 
     def __init__(self, X_train, y_train):
         self.X_train, self.y_train = X_train, y_train
-        self.classifier = MLPClassifier(solver='lbfgs', random_state=42, max_iter=100, alpha=0.01, hidden_layer_sizes=(30, 30))
+        self.classifier = LogisticRegression()
         self.classifier.fit(X_train, y_train)
+
+    def predict(self, X):
+        return self.classifier.predict(X)
