6c55e383fb268403707c6f0246c492cfec10df90
Paweł Harasiuk <pharasiuk01@gmail.com>
2024-02-12 15:45:10 +0100

    Updated preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -7,13 +7,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 class DataPreprocessing:
     def __init__(self):
         self.df = self.read_data()
-        self.tfidf_vectorizer = TfidfVectorizer(max_features=5000)
+        self.tfidf_vectorizer = TfidfVectorizer(max_features=10000)
         self.tfidf_vectorizer.fit(self.df['text'])
 
     def read_data(self):
-        columns = ["sentiment", "timestamp", "date", "query", "user", "text"]
-        df = pd.read_csv("data/training.1600000.processed.noemoticon.csv", header=None, names=columns,
+        columns = ["text", "category"]
+        df = pd.read_csv("data/Twitter_Data.csv", header=None, names=columns,
                          encoding='ISO-8859-1')
+        df = df.dropna(subset=['text', 'category'])
         return df
 
     def preprocess_text(self, text):
@@ -25,12 +26,11 @@ class DataPreprocessing:
 
     def split_data(self):
         self.df['text'] = self.df['text'].apply(self.preprocess_text)
-        X_train, X_val, y_train, y_val = train_test_split(self.df['text'], self.df['sentiment'], test_size=0.2,
+        X_train, X_val, y_train, y_val = train_test_split(self.df['text'], self.df['category'], test_size=0.2,
                                                           random_state=42)
-        tfidf_vectorizer = TfidfVectorizer(max_features=5000)
 
-        X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
-        X_val_tfidf = tfidf_vectorizer.transform(X_val)
+        X_train_tfidf = self.tfidf_vectorizer.fit_transform(X_train)
+        X_val_tfidf = self.tfidf_vectorizer.transform(X_val)
 
         return X_train_tfidf, X_val_tfidf, y_train, y_val
 
@@ -38,4 +38,3 @@ class DataPreprocessing:
         preprocessed_text = self.preprocess_text(text)
         vectorized_text = self.tfidf_vectorizer.transform([preprocessed_text])
         return vectorized_text
-
