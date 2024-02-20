9688ffb6390df4d7350cdb6e760efd43e244dcb7
Paweł Harasiuk <pharasiuk01@gmail.com>
2024-02-20 12:33:26 +0100

    Update and rename request.sh to create_raport.sh

--- create_raport.sh
+++ create_raport.sh
@@ -4,5 +4,5 @@ curl -sX POST \
   -H "Content-Type: application/json" \
   -d "{ \"url\": \"$URL\"}" \
   https://1le09i20al.execute-api.eu-central-1.amazonaws.com/development \
-  | jq '.' > my_file.json
+  | jq '.' > raport.json
 
